bdd8b9e02921d9bd721d10510fea3d80c05fa0f7
Hengxi <jinhengxi93@gmail.com>
2022-11-04 15:19:00 +0900

    ADD : loadable/component, code splitting

--- client.tsx
+++ client.tsx
@@ -1,8 +1,10 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import axios from 'axios';
 
 import App from './layouts/App';
