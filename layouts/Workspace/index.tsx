176084add7d503c76e00a564d38992dd123d1a71
Hengxi <jinhengxi93@gmail.com>
2022-11-13 14:27:49 +0900

    ADD : gravatar

---
+++ layouts/Workspace/index.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useCallback } from 'react';
+
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import useSWR from 'swr';
+import { Redirect } from 'react-router';
+
+import {
+  AddButton,
+  Channels,
+  Chats,
+  Header,
+  LogOutButton,
+  MenuScroll,
+  ProfileImg,
+  ProfileModal,
+  RightMenu,
+  WorkspaceButton,
+  WorkspaceModal,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('http://localhost:3095/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        mutate(false, false);
+      });
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src="" alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
+      <button onClick={onLogout}>로그아웃</button>
+      {children}
+    </div>
+  );
+};
+
+export default Workspace;
